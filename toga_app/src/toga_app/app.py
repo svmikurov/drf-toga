12a67da9a891fe23500f6adfd0448b1579c5949a
Sergei Mikurov <454004@mail.ru>
2024-08-17 17:17:17 +0500

    Add toga_app

---
+++ toga_app/src/toga_app/app.py
@@ -0,0 +1,40 @@
+"""My first application."""
+
+import httpx
+import toga
+from toga.style import Pack
+
+
+class TogaApp(toga.App):
+
+    def startup(self):
+        """Construct and show the Toga application."""
+        main_box = toga.Box()
+
+        button = toga.Button(
+            text='Greeting',
+            style=Pack(padding=(0, 5)),
+            on_press=self.say_hello,
+        )
+
+        main_box.add(button)
+
+        self.main_window = toga.MainWindow(title=self.formal_name)
+        self.main_window.content = main_box
+        self.main_window.show()
+
+    def say_hello(self, widget):
+        """Get response from drf_project."""
+        with httpx.Client() as client:
+            response = client.get('http://127.0.0.1:8000/api/v1/hello')
+
+        payload = response.json()
+
+        self.main_window.info_dialog(
+            title='Greeting',
+            message=payload.get('greeting'),
+        )
+
+
+def main():
+    return TogaApp()
