812a2099a62fd81fc01bc1b183e7a0c6da477577
Sergei Mikurov <454004@mail.ru>
2024-08-25 18:28:23 +0500

    feat(toga): add auth

--- toga-app/src/toga_app/contrib/http_requests.py
+++ toga-app/src/toga_app/contrib/http_requests.py
@@ -2,29 +2,31 @@
 
 import httpx
 
+auth = httpx.BasicAuth('admin', '1q2s3d4r')
+
 
 def send_get_request(url: str) -> list[dict]:
     """Send a `GET` request."""
-    with httpx.Client() as client:
+    with httpx.Client(auth=auth) as client:
         response = client.get(url=url)
     return response.json()
 
 
 def send_post_request(url: str, data: dict) -> str:
     """Send a `POST` request."""
-    with httpx.Client() as client:
+    with httpx.Client(auth=auth) as client:
         response = client.post(url, json=data)
     return str(response.json())
 
 def send_patch_request(url: str, data: dict) -> str:
     """Send a `POST` request."""
-    with httpx.Client() as client:
+    with httpx.Client(auth=auth) as client:
         response = client.patch(url, json=data)
     return str(response.json())
 
 
 def send_delete_request(url: str) -> str:
     """Send a `DELETE` request."""
-    with httpx.Client() as client:
+    with httpx.Client(auth=auth) as client:
         response = client.delete(url)
     return str(response)
