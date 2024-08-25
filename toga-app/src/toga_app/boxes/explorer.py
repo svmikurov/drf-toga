c21a073c1dbacd7f1a78880eeaadd1f9b1fffff5
Sergei Mikurov <454004@mail.ru>
2024-08-25 18:23:21 +0500

    feat(toga): add explorer

---
+++ toga-app/src/toga_app/boxes/explorer.py
@@ -0,0 +1,29 @@
+"""Explore Liskov Substitution Principle."""
+import toga
+from toga.style import Pack
+
+DEFAULT_STYLE = Pack()
+
+
+class ExploreLSPButton(toga.Button):
+    """Explore LSP Box."""
+
+    def __init__(self, *args, style=None, **kwargs):
+        self.style = style if style else DEFAULT_STYLE
+        super().__init__(*args, style, **kwargs)
+
+
+class ExploreLSPBox(toga.Box):
+    """Explore LSP Box."""
+
+    def __init__(self, *args, **kwargs):
+        super().__init__()
+
+        # Define Button
+        self.btn_move_main_box = ExploreLSPButton(
+                'На главную',
+                on_press=lambda _: self.app.to_main_box(),
+            )
+
+        # Add Button into ExploreLSPBox
+        self.add(self.btn_move_main_box)
