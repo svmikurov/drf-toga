28c700a48c8778f97764dd98d6333642ad051eb8
Sergei Mikurov <454004@mail.ru>
2024-08-27 16:40:54 +0500

    feat(drf): add pagination

--- drf_project/config/settings.py
+++ drf_project/config/settings.py
@@ -162,4 +162,7 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.BasicAuthentication',
         'rest_framework.authentication.SessionAuthentication',
     ],
+    # Pagination
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 5
 }
